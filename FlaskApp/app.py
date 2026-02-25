ddc9b9da5b3b2b19f15c17dd94bfe5c041854178
Your Name <you@example.com>
2026-02-26 05:50:54 +0800

    new

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, session, redirect
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -7,6 +7,8 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+app.secret_key = 'why would I tell you my secret key?'
 
 mysql = MySQL(app)
 @app.route('/api/signup',methods=['POST'])
@@ -45,12 +47,35 @@ def showSignin():
 
 @app.route('/api/validateLogin',methods=['POST'])
 
+@app.route('/api/validateLogin', methods=['POST'])
 def validateLogin():
     try:
         _username = request.form['inputEmail']
         _password = request.form['inputPassword']
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin', (_username,))
+        data = cursor.fetchall()
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]), _password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html', error='Wrong Email address or Password')
     except Exception as e:
-        return render_template('error.html',error = str(e))
+        return render_template('error.html', error=str(e))
+
+@app.route('/userhome')
+def userHome():
+    if session.get('user'):
+        return render_template('userhome.html')
+    else: 
+        return render_template('error.html',error = 'Unauthorized Access')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
 
 @app.route("/")
 def main():
